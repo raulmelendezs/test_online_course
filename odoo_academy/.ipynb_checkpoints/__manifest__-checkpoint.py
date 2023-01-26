bf9b96f458c6eabb52b981634f78da547947433b
raulmelendezs <raulmelendezs@gmail.com>
2023-01-26 17:43:32 +0000

    [ADD] Basic Module

--- odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -21,7 +21,9 @@
     
     'depends': ['base'],
     'data': [
-    
+        'security/academy_security.xml',
+        'security/ir.model.access.csv',
+        'views/academy_menuitems.xml',
     ],
     'demo':[
         'demo/academy_demo.xml',
