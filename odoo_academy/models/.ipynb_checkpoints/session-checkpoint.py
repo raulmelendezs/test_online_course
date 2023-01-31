b5adf4ba52fc0db0f4b5703ba98228de8b12dd8d
raulmelendezs <raulmelendezs@gmail.com>
2023-01-31 21:02:03 +0000

    [ADD] Basic Module

--- odoo_academy/models/.ipynb_checkpoints/session-checkpoint.py
+++ odoo_academy/models/.ipynb_checkpoints/session-checkpoint.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from datetime import timedelta
 
 class Session(models.Model):
     _name='academy.session'
@@ -16,3 +17,32 @@ class Session(models.Model):
     instructor_id = fields.Many2one(comodel_name='res.partner', string='Instructor')
     
     students_ids = fields.Many2many(comodel_name='res.partner', string='Students')
+    
+    #--Sesion video 7 
+    start_date = fields.Date(string='Start Date',
+                             default=fields.Date.today)
+    
+    duration = fields.Integer(string='Session Days',
+                              defaul=1)
+    
+    end_date = fields.Date(string='End Date',
+                           compute='_compute_end_date',
+                          inverse='_inverse_end_date',
+                          store=True)
+    
+    @api.depends('start_date','duration')
+    def _compute_end_date(self):
+        for record in self:
+            if not (record.start_date and record.duration):
+                record.end_date = record.start_date
+            else:
+                duration = timedelta(days=record.duration)
+                record.end_date = record.start_date + duration
+                
+    def _inverse_end_date(self):
+        for record in self:
+            if record.start_date and record.end_date:
+                record.duration = (record.end_date - record.start_date).days + 1
+            else:
+                continue
+    
