f50512d7c4768dda32e6b01de95aae2bcafb9d87
raulmelendezs <raulmelendezs@gmail.com>
2023-02-01 22:03:52 +0000

    [ADD] Basic Module

--- odoo_academy/models/course.py
+++ odoo_academy/models/course.py
@@ -21,7 +21,7 @@ class Course(models.Model):
     # Fields for Pricing
     base_price = fields.Float(string='Base Price', digits='Product Price', default=0.00)
     additional_fee = fields.Float(string='Additional Fee', digits='Product Price', default=10.00)
-    total_price = fields.Float(string='Total Price', digits='Product Price', compute='_compute_total_price', readonly=True)
+    total_price = fields.Float(string='Total Price', digits='Product Price', compute='_compute_total_price', readonly=True, store=True)
     
     sessions_id = fields.One2many(comodel_name='academy.session',
                                  inverse_name='course_id',
@@ -32,10 +32,10 @@ class Course(models.Model):
     @api.depends('base_price', 'additional_fee')
     def _compute_total_price(self):
         for record in self:
-            if self.base_price < 0.00:
+            if record.base_price < 0.00:
                 raise UserError(('Base Price cannot be set as Negative.'))
             
-            self.total_price = self.base_price + self.additional_fee
+            record.total_price = record.base_price + record.additional_fee
     
     # --------------------------------------- Constrains Methods ----------------------------------
     @api.constrains('additional_fee')
